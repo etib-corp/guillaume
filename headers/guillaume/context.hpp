00d9610720c11d567b2fc30003dff3c46c7780f3
XAVIER Clément Antoine <clement-antoine.xavier@epitech.eu>
2025-07-08 14:53:55 +0400

    Chore: Format files

--- headers/guillaume/context.hpp
+++ headers/guillaume/context.hpp
@@ -34,34 +34,34 @@ public:
         _renderer->init("Guillaume");
         _renderer->register_event_handler(
             [this](std::unique_ptr<Eventable> event) {
-            if (auto quit_event = std::dynamic_pointer_cast<QuitEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
-                std::cout << "Quit event received." << std::endl;
-                _renderer->set_running(false);
-            }
+                if (auto quit_event = std::dynamic_pointer_cast<QuitEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
+                    std::cout << "Quit event received." << std::endl;
+                    _renderer->set_running(false);
+                }
             },
             EventType::QUIT);
 
         _renderer->register_event_handler(
             [this](std::unique_ptr<Eventable> event) {
-            if (auto keyboard_event = std::dynamic_pointer_cast<KeyboardEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
-                std::cout << "Keyboard event received: " << keyboard_event->to_string() << std::endl;
-            }
+                if (auto keyboard_event = std::dynamic_pointer_cast<KeyboardEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
+                    std::cout << "Keyboard event received: " << keyboard_event->to_string() << std::endl;
+                }
             },
             EventType::KEYBOARD_EVENT);
 
         _renderer->register_event_handler(
             [this](std::unique_ptr<Eventable> event) {
-            if (auto mouse_event = std::dynamic_pointer_cast<MouseEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
-                std::cout << "Mouse event received: " << mouse_event->to_string() << std::endl;
-            }
+                if (auto mouse_event = std::dynamic_pointer_cast<MouseEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
+                    std::cout << "Mouse event received: " << mouse_event->to_string() << std::endl;
+                }
             },
             EventType::MOUSE_EVENT);
 
         _renderer->register_event_handler(
             [this](std::unique_ptr<Eventable> event) {
-            if (auto unknown_event = std::dynamic_pointer_cast<UnknownEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
-                std::cout << "Unknown event received: " << unknown_event->to_string() << std::endl;
-            }
+                if (auto unknown_event = std::dynamic_pointer_cast<UnknownEvent>(std::shared_ptr<Eventable>(std::move(event)))) {
+                    std::cout << "Unknown event received: " << unknown_event->to_string() << std::endl;
+                }
             },
             EventType::UNKNOWN);
     }
